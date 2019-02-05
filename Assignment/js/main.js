f0c67807a6718821a598bb345f3ee5c09d477899
Ray <32021785+fluentinstroll@users.noreply.github.com>
2019-02-05 13:37:12 -0500

    Add files via upload

---
+++ Assignment/js/main.js
@@ -0,0 +1,70 @@
+/*********************************************************************************
+* WEB422 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Raymond Rambo Student ID: 122082175 Date: 
+*
+*
+********************************************************************************/ 
+
+$(function(){
+    console.log("jQuery working");
+
+    $("#teams-menu").on("click", function(event){
+        console.log("Teams menu item clicked")
+        event.preventDefault();
+        $.ajax({
+            url: "https://sleepy-waters-84661.herokuapp.com/teams",
+            type: "GET",
+            contentType: "application/json"
+        }).done(function(data){
+            $("#data").empty()
+            .html("<h3>Teams</h3>")
+            .append(JSON.stringify(data));
+        });
+    })
+
+    $("#employees-menu").on("click", function(event){
+        console.log("Employees menu item clicked")
+        event.preventDefault();
+        $.ajax({
+            url: "https://sleepy-waters-84661.herokuapp.com/employees",
+            type: "GET",
+            contentType: "application/json"
+        }).done(function(data){
+            $("#data").empty()
+            .html("<h3>Employees</h3>")
+            .append(JSON.stringify(data));
+        });
+    })
+
+    $("#projects-menu").on("click", function(event){
+        console.log("Projects menu item clicked")
+        event.preventDefault();
+        $.ajax({
+            url: "https://sleepy-waters-84661.herokuapp.com/projects",
+            type: "GET",
+            contentType: "application/json"
+        }).done(function(data){
+            $("#data").empty()
+            .html("<h3>pProjects</h3>")
+            .append(JSON.stringify(data));
+        });
+    })
+
+    $("#positions-menu").on("click", function(event){
+        console.log("Positions menu item clicked")
+        event.preventDefault();
+        $.ajax({
+            url: "https://sleepy-waters-84661.herokuapp.com/positions",
+            type: "GET",
+            contentType: "application/json"
+        }).done(function(data){
+            $("#data").empty()
+            .html("<h3>Positions</h3>")
+            .append(JSON.stringify(data));
+        });
+    })
+})
